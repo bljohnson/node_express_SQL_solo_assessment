a4de9c1e14db6e022016a279c367f3c7a00b3a14
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-06-17 16:13:32 -0500

    Functionality working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,10 +2,20 @@ console.log('script.js is sourced!');
 
 $(document).ready(function() {
   console.log('jQuery is working!');
+
+  $.ajax({
+    type: 'GET',
+    url: '/getZoo',
+    success: function(data){
+    console.log('Got the zoo:' + displayZoo(data));
+    } // end success
+  }); //end ajax
+
   $('#add').on('click', function () {
+    event.preventDefault();
     // assemble an object (always need to do for a post call)
     var animal = $('#animal').val();
-    console.log(animal);
+    console.log('animal type added:' + animal);
     var objectToSend = {
       'animalType': animal,
     }; // end object being sent
@@ -15,6 +25,29 @@ $(document).ready(function() {
       url: '/postNewAnimal',
       data: objectToSend
     }); // end ajax request
-    $('#animal').empty();
+    $('#animal').val(''); // empty input field after previous animal added
   }); // end add click function
+
+  $('#getZoo').on('click', function(){
+    event.preventDefault();
+    console.log('clicked to get the zoo');
+   $.ajax({
+     type: 'GET',
+     url: '/getZoo',
+     success: function(data){
+     console.log('Got the zoo:' + displayZoo(data));
+     } // end success
+   }); //end ajax
+   $('#outputDiv').empty();
+ });
+
+
+ function displayZoo (zoo) {
+    console.log( 'in displayZoo:' + zoo );
+    for(i=0; i<zoo.length; i++) {
+      var currentZooInventory = "<p>Animal Type: " + zoo[i].animal_type + ", Quantity: " + zoo[i].quantity + "</p>";
+      $('#outputDiv').append(currentZooInventory);
+    } // end for loop
+  } // end displayZoo
+
 }); // end doc ready function
--- server/app.js
+++ server/app.js
@@ -18,16 +18,35 @@ app.use(express.static('public'));
 // resolved path to index.html
 // base URL
 // logs to Atom terminal since coming from server side
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   console.log('in base URL');
   console.log(getQuantity(1,100)); // works
   res.sendFile( path.resolve( 'views/index.html' ) ); // gets this path
 });
+
+app.get('/getZoo', function (req, res){
+  console.log('in getZoo URL');
+  // this wil hold our results
+    var stock = [];
+    pg.connect(connectionString, function (err, client, done){
+      // get all animals and store in query variable
+      var currentZoo = client.query('SELECT animal_type, quantity FROM animal_inventory;');
+      console.log("current zoo: " + currentZoo);
+      // push each row in query into our results array
+      var rows = 0;
+      currentZoo.on('row', function (row) {
+        stock.push(row);
+      }); // end query push
+      currentZoo.on('end', function (){
+        return res.json(stock);
+      });
+    }); // end connect
+  }); // end app.get
 
 // post route (requires the urlencodedParser INJECTION between route and function)
 app.post('/postNewAnimal', urlencodedParser, function(req, res){
   console.log('in postNewAnimal URL:' + req.body.animalType);
   pg.connect(connectionString, function(err, client, done){
     client.query('INSERT INTO animal_inventory (animal_type, quantity) VALUES ($1, $2)', [req.body.animalType, getQuantity(1,100)]);
-  });
-});
+  }); // end connect
+}); // end app.post
